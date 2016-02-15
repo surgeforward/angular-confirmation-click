c655ca4033fb7fe246ef6b98e9accbe35467b2ab
maniacally <alan@Alans-Mac-mini.local>
2016-02-15 21:31:56 +0100

    adding multiple config options

--- confirmationClickDirective.js
+++ confirmationClickDirective.js
@@ -23,13 +23,21 @@
                     link: function (scope, element, attrs) {
                         element.bind('click', function () {
                             var message = attrs.ngConfirmationMessage || "Are you sure ?";
+                            var yesButtonText = attrs.ngConfirmationYesText || "Yes";
+                            var cancelButtonText = attrs.ngConfirmationCancelText || "Cancel";
+                            var backdrop = !!attrs.ngConfirmationBackgrdop;
+                            var animation = !!attrs.ngConfirmationAnimation;
+                            var size = 'md';                            
 
                             var modalHtml = 
                                 (attrs.ngConfirmationTitle? ('<div class="modal-header"><h3>' + attrs.ngConfirmationTitle + '</h3></div>') : '')
                                 + '<div class="modal-body"><h4>' + message + '</h4></div>';
-                            modalHtml += '<div class="modal-footer"><button class="btn btn-primary" ng-click="ok()">Yes</button><button class="btn btn-warning" ng-click="cancel()">Cancel</button></div>';
+                            modalHtml += '<div class="modal-footer"><button class="btn btn-primary" ng-click="ok()">' + yesButtonText + '</button><button class="btn btn-warning" ng-click="cancel()">' + cancelButtonText + '</button></div>';
 
                             var modalInstance = $modal.open({
+                                backdrop: true,                                
+                                animation: false,
+                                size: 'xl',
                                 template: modalHtml,
                                 controller: ModalInstanceCtrl
                             });
